55b62e9170d18e9265a8dfca56540157e98d5a53
Cam McOrist <cameronmcorist@gmail.com>
2024-11-23 18:05:58 +1100

    wrap in CTA div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Link href="https://rupertfund.square.site/">DONATE NOW!</Link>
+        <div class="ctas">
+          <Link href="https://rupertfund.square.site/">DONATE NOW!</Link>
+        </div>
         <ul>
           <li>Welcome to Rupert&apos;s website!</li>
           <li>We&apos;ll be getting this up and running in no time.</li>
