d6b47b99ad87a5d401378cbf510aba23662899e7
cam <cameronmcorist@gmail.com>
2024-11-23 13:20:57 +1100

    ul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
+        <ul>
           <li>Welcome to Rupert&apos;s website!</li>
           <li>We&apos;ll be getting this up and running in no time.</li>
-        </ol>
+        </ul>
       </main>
     </div>
   );
