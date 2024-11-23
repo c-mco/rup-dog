b72798fc6fed0556250b91bdb41e63f095fef1d6
Cam McOrist <cameronmcorist@gmail.com>
2024-11-23 17:22:13 +1100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from "./page.module.css";
-
+import Link from 'next/link'
+ 
+ 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Link href="https://rupertfund.square.site/">DONATE NOW!</Link>
         <ul>
           <li>Welcome to Rupert&apos;s website!</li>
           <li>We&apos;ll be getting this up and running in no time.</li>
