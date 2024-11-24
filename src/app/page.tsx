898614c195f6f91314fd810ac59a776253829201
cam <cameronmcorist@gmail.com>
2024-11-24 12:10:19 +1100

    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import styles from "./page.module.css";
-import Link from 'next/link'
- 
- 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className={styles.page}>
