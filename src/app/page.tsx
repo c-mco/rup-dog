e83459ec80c71919099740a4519651843a575fc7
cam <cameronmcorist@gmail.com>
2024-11-24 15:41:42 +1100

    lots of styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,69 @@
-import styles from "./page.module.css";
-import Link from "next/link";
+/* page.tsx */
+
+import "./globals.css";
+import Image from "next/image";
+import sadruImage from "./sadru.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.ctas}>
-          <Link href="https://rupertfund.square.site/">DONATE NOW!</Link>
+    <div className="container">
+      <div className="main-content">
+        <p>
+          🎉 It&apos;s a PAW-ty! Join us at <strong>Foreigner Brewery</strong>{" "}
+          in Brunswick to celebrate the amazing Rupert turning{" "}
+          <strong>8 years young</strong>!
+        </p>
+        <p>
+          🐾 Our big boy recently had surgery (goodbye, tail tumor, 👋), and the
+          vet bills were no joke. But don&apos;t worry—Rupert&apos;s still
+          wagging what he&apos;s got left!
+        </p>
+        <p>
+          To help out, we&apos;re throwing a fundraiser with exclusive Rupert
+          merch, fun surprises, and plenty of brews. All proceeds go to covering
+          his medical bills—and maybe a few extra dog treats.
+        </p>
+        <p>
+          Don&apos;t miss the chance to support the best boy in town. Be there
+          or be...a sad dog pun. 🐶
+        </p>
+      </div>
+      <div className="imageContainer">
+        <Image
+          src={sadruImage}
+          alt="Sadru the Dog"
+          layout="responsive"
+          width={500}
+          height={500}
+        />
+      </div>
+      <footer className="footer">
+        <div className="socials">
+          <a
+            href="https://www.instagram.com/theokayestdane"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} /> @theokayestdane
+          </a>
+          <a
+            href="https://www.instagram.com/foreignerbrewery"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} /> @foreignerbrewing
+          </a>
+          <a
+            href="https://www.facebook.com/foreignerbrewery"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} /> @foreignerbrewing
+          </a>
         </div>
-        <ul>
-          <li>Welcome to Rupert&apos;s website!</li>
-          <li>We&apos;ll be getting this up and running in no time.</li>
-        </ul>
-      </main>
+      </footer>
     </div>
   );
 }
